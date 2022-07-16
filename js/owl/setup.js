fadac6ee67a3a084a25363bc18188eb8622904c5
Geladin <higor.ribeiro13@gmail.com>
2022-07-16 00:26:10 -0300

    pronto

---
+++ js/owl/setup.js
@@ -0,0 +1,324 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        },
+        2000:{
+            items:10
+        }
+    }
+})
+
+function redirecionar_sims_yt(){
+    window.open("https://www.youtube.com/watch?v=z00mK3Pxc8w&ab_channel=TheSims");
+}
+function redirecionar_sims_pag(){
+    window.open("https://www.ea.com/pt-br/games/the-sims/the-sims-4");
+}
+
+//Carrossel Nintendo
+function redirecionar_zelda(){
+    window.open("https://www.zelda.com/breath-of-the-wild/");
+}
+function redirecionar_zelda_yt(){
+    window.open("https://www.youtube.com/watch?v=zw47_q9wbBE&ab_channel=Nintendo");
+}
+
+function redirecionar_odyssey(){
+    window.open("https://supermario.nintendo.com/");
+}
+function redirecionar_odyssey_yt(){
+    window.open("https://www.youtube.com/watch?v=wGQHQc_3ycE&ab_channel=Nintendo");
+}
+
+function redirecionar_party(){
+    window.open("https://www.nintendo.com/store/products/super-mario-party-switch/");
+}
+function redirecionar_party_yt(){
+    window.open("https://www.youtube.com/watch?v=MDTTZisOtX0&ab_channel=Nintendo");
+}
+
+function redirecionar_kart(){
+    window.open("https://www.nintendo.com/store/products/mario-kart-8-deluxe-switch/");
+}
+function redirecionar_kart_yt(){
+    window.open("https://www.youtube.com/watch?v=tKlRN2YpxRE&ab_channel=Nintendo");
+}
+
+function redirecionar_animal(){
+    window.open("https://www.nintendo.com/store/products/animal-crossing-new-horizons-switch/");
+}
+function redirecionar_animal_yt(){
+    window.open("https://www.youtube.com/watch?v=_3YNL0OWio0&ab_channel=Nintendo");
+}
+
+function redirecionar_smash(){
+    window.open("https://www.nintendo.com/store/products/super-smash-bros-ultimate-switch/");
+}
+function redirecionar_smash_yt(){
+    window.open("https://www.youtube.com/watch?v=L93H7YC-83o&ab_channel=Nintendo");
+}
+
+function redirecionar_mansion(){
+    window.open("https://www.nintendo.com/store/products/luigis-mansion-3-switch/");
+}
+function redirecionar_mansion_yt(){
+    window.open("https://www.youtube.com/watch?v=ErJcRy67gps&ab_channel=Nintendo");
+}
+
+function redirecionar_kong(){
+    window.open("https://www.nintendo.com/store/products/donkey-kong-country-tropical-freeze-switch/");
+}
+function redirecionar_kong_yt(){
+    window.open("https://www.youtube.com/watch?v=_5p0SiWHwvw&ab_channel=Nintendo");
+}
+
+function redirecionar_slaptoon(){
+    window.open("https://www.nintendo.com/store/products/splatoon-2-switch/");
+}
+function redirecionar_slaptoon_yt(){
+    window.open("https://www.youtube.com/watch?v=qN4w5D2tzME&ab_channel=Nintendo");
+}
+
+function redirecionar_3D(){
+    window.open("https://www.nintendo.com/store/products/super-mario-3d-world-plus-bowsers-fury-switch/");
+}
+function redirecionar_3D_yt(){
+    window.open("https://www.youtube.com/watch?v=5nW9o6M5zFo&ab_channel=Nintendo");
+}
+
+//Carrossel Blizzard
+function redirecionar_wow_yt(){
+    window.open("https://worldofwarcraft.com/pt-br/");
+}
+function redirecionar_wow_yt(){
+    window.open("https://www.youtube.com/watch?v=vlVSJ0AvZe0&ab_channel=WorldofWarcraft");
+}
+
+function redirecionar_over(){
+    window.open("https://playoverwatch.com/pt-br/");
+}
+function redirecionar_over_yt(){
+    window.open("https://www.youtube.com/watch?v=FqnKB22pOC0&ab_channel=PlayOverwatch");
+}
+
+function redirecionar_heroes_yt(){
+    window.open("https://heroesofthestorm.com/pt-br/");
+}
+function redirecionar_heroes(){
+    window.open("https://www.youtube.com/watch?v=0ecv0bT9DEo&ab_channel=HeroesoftheStorm");
+}
+
+function redirecionar_diablo_yt(){
+    window.open("https://www.youtube.com/watch?v=M2TpNQhfkp4&ab_channel=Wacraft3Archive");
+}
+function redirecionar_diablo(){
+    window.open("https://us.diablo3.blizzard.com/pt-br/");
+}
+
+function redirecionar_diablo2(){
+    window.open("https://diablo2.blizzard.com/pt-br/");
+}
+function redirecionar_diablo2_yt(){
+    window.open("https://www.youtube.com/watch?v=InPmwg-0ByA&ab_channel=Diablo");
+}
+
+function redirecionar_diablo1(){
+    window.open("https://www.diablowiki.net/Diablo_I");
+}
+function redirecionar_diablo1_yt(){
+    window.open("https://www.youtube.com/watch?v=o_Kr5i5F43U&ab_channel=BlizzardArchive");
+}
+
+function redirecionar_frozen(){
+    window.open("https://en.wikipedia.org/wiki/Warcraft_III:_The_Frozen_Throne");
+}
+function redirecionar_frozen_yt(){
+    window.open("https://www.youtube.com/watch?v=UgTmcnR3iaY&ab_channel=DLCompare");
+}
+
+function redirecionar_caos(){
+    window.open("https://en.wikipedia.org/wiki/Warcraft_III:_Reign_of_Chaos");
+}
+function redirecionar_caos_yt(){
+    window.open("https://www.youtube.com/watch?v=0UWc93t09ck&ab_channel=Ryan%27sCinematics");
+}
+
+function redirecionar_warcraft2(){
+    window.open("https://en.wikipedia.org/wiki/Warcraft_II:_Tides_of_Darkness");
+}
+function redirecionar_warcraft2_yt(){
+    window.open("https://www.youtube.com/watch?v=hizMHgJVGDU");
+}
+
+function redirecionar_warcraft(){
+    window.open("https://en.wikipedia.org/wiki/Warcraft:_Orcs_%26_Humans");
+}
+function redirecionar_warcraft_yt(){
+    window.open("https://www.youtube.com/watch?v=K5kqEH8fg1U&ab_channel=ThePhantomGen-Ei");
+}
+
+//Carrossel Sony
+function redirecionar_zero(){
+    window.open("https://www.playstation.com/pt-br/games/horizon-zero-dawn/");
+}
+function redirecionar_zero_yt(){
+    window.open("https://www.youtube.com/watch?v=wzx96gYA8ek");
+}
+
+function redirecionar_death(){
+    window.open("https://www.playstation.com/pt-br/games/death-stranding/");
+}
+function redirecionar_death_yt(){
+    window.open("https://www.youtube.com/watch?v=Mpn-MC2B6Zc");
+}
+ 
+function redirecionar_death(){
+    window.open("https://www.playstation.com/pt-br/games/death-stranding/");
+}
+function redirecionar_death_yt(){
+    window.open("https://www.youtube.com/watch?v=Mpn-MC2B6Zc");
+} 
+
+function redirecionar_god(){
+    window.open("https://www.playstation.com/pt-br/games/god-of-war/");
+}
+function redirecionar_god_yt(){
+    window.open("https://www.youtube.com/watch?v=u9wM2Zdeloc&ab_channel=PlayStation");
+} 
+
+function redirecionar_crash(){
+    window.open("https://www.playstation.com/pt-br/games/crash-bandicoot-4-its-about-time/");
+}
+function redirecionar_crash_yt(){
+    window.open("https://www.youtube.com/watch?v=pDBC8QrN9hw&ab_channel=PlayStation");
+} 
+
+function redirecionar_days(){
+    window.open("https://www.playstation.com/pt-br/games/days-gone/");
+}
+function redirecionar_days_yt(){
+    window.open("https://www.youtube.com/watch?v=FKtaOY9lMvM&ab_channel=PlayStation");
+} 
+
+function redirecionar_last1(){
+    window.open("https://www.playstation.com/pt-br/games/the-last-of-us-part-i/");
+}
+function redirecionar_last1_yt(){
+    window.open("https://www.youtube.com/watch?v=WxjeV10H1F0&ab_channel=PlayStation");
+}
+
+function redirecionar_last2(){
+    window.open("https://www.playstation.com/pt-br/games/the-last-of-us-part-ii/");
+}
+function redirecionar_last2_yt(){
+    window.open("https://www.youtube.com/watch?v=psit-4r_hys&ab_channel=PlayStation");
+}
+
+function redirecionar_detroit(){
+    window.open("https://www.playstation.com/pt-br/games/detroit-become-human/");
+}
+function redirecionar_detroit_yt(){
+    window.open("https://www.youtube.com/watch?v=8a-EObAhYrg&ab_channel=PlayStation");
+}
+
+function redirecionar_blood(){
+    window.open("https://www.playstation.com/pt-br/games/bloodborne/");
+}
+function redirecionar_blood_yt(){
+    window.open("https://www.youtube.com/watch?v=3AR_IIYFCh0&ab_channel=PlayStationBrasil");
+}
+
+function redirecionar_ghost(){
+    window.open("https://www.playstation.com/pt-br/games/ghost-of-tsushima/");
+}
+function redirecionar_ghost_yt(){
+    window.open("https://www.youtube.com/watch?v=bzi1WsRCf-s&ab_channel=PlayStationBrasil");
+}
+
+//Carrosel Xbox
+function redirecionar_sea(){
+    window.open("https://www.xbox.com/pt-br/games/store/sea-of-thieves/9p2n57mc619k");
+}
+function redirecionar_sea_yt(){
+    window.open("https://www.youtube.com/watch?v=r5JIBaasuE8&ab_channel=SeaofThieves");
+}
+
+function redirecionar_halo(){
+    window.open("https://www.xbox.com/pt-br/games/store/halo-5-guardians/brrc2bp0g9p0");
+}
+function redirecionar_halo_yt(){
+    window.open("https://www.youtube.com/watch?v=Rh_NXwqFvHc&ab_channel=Xbox");
+}
+
+function redirecionar_forza(){
+    window.open("https://www.youtube.com/watch?v=CstDDE4YC5c&ab_channel=XboxBR");
+}
+function redirecionar_forza_yt(){
+    window.open("https://www.youtube.com/watch?v=Rh_NXwqFvHc&ab_channel=Xbox");
+}
+
+function redirecionar_killer(){
+    window.open("https://www.xbox.com/pt-br/games/store/killer-instinct/bp4s3xljg78m");
+}
+function redirecionar_killer_yt(){
+    window.open("https://www.youtube.com/watch?v=Z1FgmHRPtLU&ab_channel=Xbox");
+}
+
+function redirecionar_ryse(){
+    window.open("https://www.xbox.com/pt-br/games/store/ryse-son-of-rome/c10qtnc7jwnj");
+}
+function redirecionar_ryse_yt(){
+    window.open("https://www.youtube.com/watch?v=QNO9n9TthZg&ab_channel=outsidexbox");
+}
+
+function redirecionar_break(){
+    window.open("https://www.xbox.com/pt-br/games/store/quantum-break/bpndkqn84n6l");
+}
+function redirecionar_brak_yt(){
+    window.open("https://www.youtube.com/watch?v=ruY1eT9bXiw&ab_channel=IGN");
+}
+
+function redirecionar_state(){
+    window.open("https://www.xbox.com/pt-br/games/store/state-of-decay-year-one-survival-edition/c4hxgz1x0881");
+}
+function redirecionar_state_yt(){
+    window.open("https://www.youtube.com/watch?v=edVYXynxzio&ab_channel=GameSpot");
+}
+
+function redirecionar_gears(){
+    window.open("https://www.xbox.com/pt-br/games/store/gears-5/c2kdnlt2h7dm");
+}
+function redirecionar_gears_yt(){
+    window.open("https://www.youtube.com/watch?v=h35_KOgslkM&ab_channel=Xbox");
+}
+
+function redirecionar_war3(){
+    window.open("https://www.xbox.com/pt-br/games/store/gears-of-war-3/bpkdqssfq9wv");
+}
+function redirecionar_war3_yt(){
+    window.open("https://www.youtube.com/watch?v=BrjvrHaOOpw&ab_channel=IGN");
+}
+
+function redirecionar_war3(){
+    window.open("https://www.xbox.com/pt-br/games/store/gears-of-war-3/bpkdqssfq9wv");
+}
+function redirecionar_war3_yt(){
+    window.open("https://www.youtube.com/watch?v=BrjvrHaOOpw&ab_channel=IGN");
+}
+
+function redirecionar_kinect(){
+    window.open("https://en.wikipedia.org/wiki/Kinect_Adventures!");
+}
+function redirecionar_kinect_yt(){
+    window.open("https://www.youtube.com/watch?v=nee86i_vInE&ab_channel=XboxViewTV");
+}
